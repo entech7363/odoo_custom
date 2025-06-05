e4693a03cf37a7b04ee4a974e88f63d693349539
mansoor <mansoore4@gmail.com>
2025-06-05 16:17:19 +0530

    customer header moduel

---
+++ en_custom_header/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    'name': "En Custom Header",
+    'version': '1.0',
+    'author': 'AGM Global Services',
+    'description': "Custom header",
+    'depends': ['sale_management', 'sale', 'stock', 'account', 'web', 'base'],
+    'data': [
+        'reports/report_template_header.xml',
+        'reports/report_print.xml',
+        'views/en_company_view.xml',
+    ],
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+    'license': 'AGPL-3',
+}
