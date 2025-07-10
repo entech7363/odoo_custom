daa14977695633901450b33399ec71d2589ad6d5
mansoor <mansoore4@gmail.com>
2025-07-10 16:44:04 +0530

    [ADD] bi_order_line_with_sequence_number: Add sequence number to order lines

--- bi_order_line_with_sequence_number/__manifest__.py
+++ bi_order_line_with_sequence_number/__manifest__.py
@@ -2,7 +2,7 @@
 # Part of BrowseInfo. See LICENSE file for full copyright and licensing details.
 {
     'name': "Order line Sequence Number",
-    'version': '18.0.0.1',
+    'version': '18.0.0.0',
     'category': 'Extra Tools',
     'summary': "Sales order line sequence number in order line with sequence number report purchase order line sequence sale order line sequence   purchase agreement order line report sales stock order line sequence sale order line number order line sequence number report",
     'description': """
@@ -12,7 +12,7 @@
     """,
     'author': 'BROWSEINFO',
     'website': 'https://www.browseinfo.com/demo-request?app=bi_order_line_with_sequence_number&version=18&edition=Community',
-    'depends': ['base','sale_management','purchase','account','stock','mrp','purchase_requisition','pt_custom',],
+    'depends': ['base','sale_management','purchase','account','stock','mrp','purchase_requisition'],
     'data': [
         'security/access_display_order_line.xml',
         'views/order_line_view.xml',
--- bi_order_line_with_sequence_number/models/order_line.py
+++ bi_order_line_with_sequence_number/models/order_line.py
@@ -8,7 +8,7 @@ class SaleOrder(models.Model):
 class SaleOrderLine(models.Model):
     _inherit = 'sale.order.line'
 
-    sale_sequence_number = fields.Integer(string='No. ')
+    sale_sequence_number = fields.Integer(string='No. ',compute="_compute_sale_order_line_sequence")
     
     @api.depends('sale_sequence_number')
     def _compute_sale_order_line_sequence(self):
@@ -25,7 +25,7 @@ class PurchaseOrder(models.Model):
 class PurchaseOrderLine(models.Model):
     _inherit = 'purchase.order.line'
 
-    purchase_sequence_number = fields.Integer(string='No.')
+    purchase_sequence_number = fields.Integer(string='No.',compute="_compute_purchase_order_line_sequence")
 
     @api.depends('purchase_sequence_number')
     def _compute_purchase_order_line_sequence(self):
@@ -42,7 +42,7 @@ class AccountMove(models.Model):
 class AccountMoveLine(models.Model):
     _inherit = 'account.move.line'
 
-    invoice_sequence_number = fields.Char(string='No.  ')
+    invoice_sequence_number = fields.Char(string='No.  ',compute="_compute_invoice_line_sequence")
 
     @api.depends('invoice_sequence_number')
     def _compute_invoice_line_sequence(self):
@@ -56,28 +56,16 @@ class AccountMoveLine(models.Model):
 class StockMove(models.Model):
     _inherit = 'stock.move'
 
-    stock_move_sequence = fields.Integer(string='No.', compute='_compute_stock_move_sequence', store=True)
     sequence1 = fields.Char(string='Sequence Number')
-    mrp_sequence_no = fields.Integer(string='No.   ')
-    @api.depends('sale_line_id.sale_sequence_number')
-    def _compute_stock_move_sequence(self):
-        for move in self:
-            if move.sale_line_id:
-                move.stock_move_sequence = move.sale_line_id.sale_sequence_number
-            else:
-                move.stock_move_sequence = False
+    stock_move_sequence = fields.Integer(string=' No.',compute='_compute_stock_line_sequence')
+    mrp_sequence_no = fields.Integer(string='No.   ',compute='_compute_mrp_line_sequence')
 
-
-    @api.depends('picking_id.move_ids_without_package')
+    @api.depends('stock_move_sequence')
     def _compute_stock_line_sequence(self):
-        for picking in self.mapped('picking_id'):
-            number = 1
-            for move in picking.move_ids_without_package:
-                move.stock_move_sequence = number
-                number += 1
-
-
-
+        number = 1
+        for record in self.picking_id.move_ids_without_package:
+            record.stock_move_sequence = number
+            number += 1
 
     @api.depends('mrp_sequence_no')
     def _compute_mrp_line_sequence(self):
@@ -85,41 +73,7 @@ class StockMove(models.Model):
         for record in self.raw_material_production_id.move_raw_ids:
             record.mrp_sequence_no = number
             number += 1
-class StockMoveLine(models.Model):
-    _inherit = 'stock.move.line'
 
-    def _get_aggregated_product_quantities(self, strict=False, except_package=False):
-        res = super()._get_aggregated_product_quantities(strict=strict, except_package=except_package)
-
-        # Group lines similarly as Odoo does, to find the source move line for each key
-        def _group_key(line):
-            return (
-                line.product_id.id,
-                line.product_uom_id.id,
-                line.lot_id.id if strict else False,
-                line.package_id.id if not except_package else False,
-                line.result_package_id.id if not except_package else False,
-                line.owner_id.id,
-                line.location_id.id,
-                line.location_dest_id.id,
-                line.move_id.description_picking,
-            )
-
-        grouped_lines = {}
-        for line in self:
-            key = _group_key(line)
-            if key not in grouped_lines:
-                grouped_lines[key] = line  # Keep first line as reference
-
-        for key, values in res.items():
-            # key must exist in grouped_lines
-            line = grouped_lines.get(key)
-            if line:
-                values['stock_move_sequence'] = line.move_id.stock_move_sequence or ''
-            else:
-                values['stock_move_sequence'] = ''
-
-        return res
 class MrpProduction(models.Model):
     _inherit = 'mrp.production'
 
