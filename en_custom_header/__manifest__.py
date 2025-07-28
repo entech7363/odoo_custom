3ca27de031fd60cf27ecedaa145a51bae8cb328a
Alona <99014035+AlonaSunny@users.noreply.github.com>
2025-07-28 16:26:21 +0530

    report footer added

--- en_custom_header/__manifest__.py
+++ en_custom_header/__manifest__.py
@@ -1,13 +1,13 @@
 {
     'name': "En Custom Header",
-    'version': '1.0',
+    'version': '1.1',
     'author': 'AGM Global Services',
     'description': "Custom header",
     'depends': ['sale_management', 'sale', 'stock', 'account', 'web', 'base'],
     'data': [
-        'reports/report_layout_striped_header.xml',
-        'reports/report_paperformat_custom.xml',
-        'views/res_company_form_view.xml',
+        'reports/report_template_header.xml',
+        'reports/report_print.xml',
+        'views/en_company_view.xml',
     ],
     'installable': True,
     'application': False,
---
+++ en_custom_header/models/en_company.py
@@ -0,0 +1,14 @@
+from odoo import models, fields
+
+
+class ResCompany(models.Model):
+    _inherit = "res.company"
+
+    report_header_image = fields.Binary(string="Header Image", attachment=False)
+    report_footer_image = fields.Binary(string="Footer Image", attachment=False)
+
+class BaseDocumentLayout(models.TransientModel):
+    _inherit = "base.document.layout"
+
+    report_header_image = fields.Binary(string="Header Image", attachment=False)
+    report_footer_image = fields.Binary(string="Footer Image", attachment=False)
