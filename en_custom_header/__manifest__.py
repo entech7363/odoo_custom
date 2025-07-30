e579e35c2885d931df2283229f1b68a5bc59c13b
Alona <99014035+AlonaSunny@users.noreply.github.com>
2025-07-30 18:15:42 +0530

    footer change

--- en_custom_header/__manifest__.py
+++ en_custom_header/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': "En Custom Header",
-    'version': '1.2',
+    'version': '1.3',
     'author': 'AGM Global Services',
     'description': "Custom header",
     'depends': ['sale_management', 'sale', 'stock', 'account', 'web', 'base'],
