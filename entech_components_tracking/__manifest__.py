54922bb9c11b697702398d094a354112beb403f0
mansoor <mansoore4@gmail.com>
2025-08-03 12:16:15 +0530

    temporary fix for invoice not printing

--- entech_components_tracking/__manifest__.py
+++ entech_components_tracking/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'Entech Components Tracking',
-    'version': '1.9',
+    'version': '2.1',
     'category': 'Custom',
     'depends': ['base', 'sale', 'purchase', 'product', 'stock', 'account'],
     'data': [
