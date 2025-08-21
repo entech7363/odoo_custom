a96748a3f6381db548acf2482b9ec2a074b81830
AlonaSunny <alonasunny295@gmail.com>
2025-08-21 10:31:41 +0530

    changes in delivery and purchase order report

--- entech_custom/__manifest__.py
+++ entech_custom/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name':"Entech custom changes",
-    'version':'1.11',
+    'version':'1.12',
     'summary':'Creating various fields in the sale and purchase',
     'description':"module for the management of sales and purchases",
     'author': 'AGM Global Services',
