e9804857fc370594c7ea5185847daf3f2a67c7a9
AlonaSunny <alonasunny295@gmail.com>
2025-09-22 15:20:01 +0530

    bug fix of shipping address

--- entech_custom/__manifest__.py
+++ entech_custom/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name':"Entech custom changes",
-    'version':'2.0',
+    'version':'2.1',
     'summary':'Creating various fields in the sale and purchase',
     'description':"module for the management of sales and purchases",
     'author': 'AGM Global Services',
