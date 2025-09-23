31a1e76fe714ec01a933d883e5f1c8421a6f9c11
Sona <sonaeplacement@gmail.com>
2025-09-23 11:22:04 +0530

    alignment issue

--- entech_custom/__manifest__.py
+++ entech_custom/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name':"Entech custom changes",
-    'version':'2.5',
+    'version':'2.6',
     'summary':'Creating various fields in the sale and purchase',
     'description':"module for the management of sales and purchases",
     'author': 'AGM Global Services',
