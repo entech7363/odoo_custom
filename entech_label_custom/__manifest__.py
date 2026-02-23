8fb4a140a36f6c1e767ca0e64c22291d1521ea7c
mansoore4 <111485474+mansoore4@users.noreply.github.com>
2026-02-23 12:45:37 +0530

    Add files via upload

---
+++ entech_label_custom/__manifest__.py
@@ -0,0 +1,12 @@
+{
+    'name': "Entech Dymo 40x23 Label",
+    'version': '18.0.0.0.1',
+    'summary': 'Custom Dymo 40x23 product label',
+    'author': 'AGM Global Services',
+    'depends': ['product'],
+    'data': [
+        'reports/dymo_40x23_label.xml',
+    ],
+    'installable': True,
+    'application': False,
+}
