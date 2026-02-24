64ad4048b7aecc0b520995df27b572d63103cf4a
mansoor <mansoore4@gmail.com>
2026-02-24 20:13:05 +0530

    updates

--- entech_custom/__manifest__.py
+++ entech_custom/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': "Entech custom changes",
-    'version': '2.11',
+    'version': '2.12',
     'summary': 'Creating various fields in the sale and purchase',
     'description': "module for the management of sales and purchases",
     'author': 'AGM Global Services',
