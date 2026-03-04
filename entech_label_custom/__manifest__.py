c3741143014b5c57860c84f2867ac91b5cb56c04
mansoor <mansoore4@gmail.com>
2026-03-04 18:15:08 +0530

    barcode update

--- entech_label_custom/__manifest__.py
+++ entech_label_custom/__manifest__.py
@@ -5,7 +5,8 @@
     'author': 'AGM Global Services',
     'depends': ['product'],
     'data': [
-        'reports/dymo_40x23_label.xml',
+        # 'reports/dymo_40x23_label.xml',
+        'reports/product_label_report.xml',
     ],
     'installable': True,
     'application': False,
