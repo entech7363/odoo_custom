d12faa56db0629deaa632504b9f06a3455983a58
mansoor <mansoore4@gmail.com>
2026-03-05 09:11:59 +0530

    barcode update

--- entech_label_custom/__manifest__.py
+++ entech_label_custom/__manifest__.py
@@ -1,11 +1,10 @@
 {
     'name': "Entech Dymo 40x23 Label",
-    'version': '18.0.0.0.2',
+    'version': '18.0.0.0.3',
     'summary': 'Custom Dymo 40x23 product label',
     'author': 'AGM Global Services',
     'depends': ['product'],
     'data': [
-        # 'reports/dymo_40x23_label.xml',
         'reports/product_label_report.xml',
     ],
     'installable': True,
